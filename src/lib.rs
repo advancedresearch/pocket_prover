d5f0361199fb1e67acaaeaf1e904874bf4b52626
Sven Nilsen <bvssvni@gmail.com>
2017-11-05 01:49:35 +0100

    Release 0.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,62 @@
+#![deny(missing_docs)]
+
+//! # pocket_prover
+//! A fast, bounded, brute force, automatic theorem prover for first order logic
+//!
+//! For generic automated theorem proving, see [monotonic_solver](https://github.com/advancedresearch/monotonic_solver).
+//!
+//! ```rust
+//! extern crate pocket_prover;
+//!
+//! use pocket_prover::*;
+//!
+//! fn main() {
+//!     println!("Socrates is mortal: {}", prove3(|man, mortal, socrates| {
+//!         // Using `imply` because we want to prove an inference rule.
+//!         imply(
+//!             // Premises.
+//!             and(
+//!                 // All men are mortal.
+//!                 imply(man, mortal),
+//!                 // Socrates is a man.
+//!                 imply(socrates, man),
+//!             ),
+//!
+//!             // Conclusion.
+//!             imply(socrates, mortal)
+//!         )
+//!     }));
+//! }
+//! ```
+//!
+//! ### Motivation
+//!
+//! The motivation is to provide the analogue of a "pocket calculator",
+//! but for logic, therefore called a "pocket prover".
+//!
+//! This library uses an approach that is simple to implement from scratch in a low level language.
+//!
+//! This is useful in cases like:
+//!
+//! - Study logic without the hurdle of doing manual proofs
+//! - Checking your understanding of logic
+//! - Verify that logicians are wizards and not lizards
+//! - Due to a series of unfortunate events, you got only 24 hours to learn logic and just need the gist of it
+//! - Memorizing source code for situations like [The Martian](http://www.imdb.com/title/tt3659388/)
+//! - A tiny mistake and the whole planet blows up (e.g. final decisions before the AI singularity and you need to press the right buttons)
+//!
+//! ### Implementation
+//!
+//! This library uses brute-force to check proofs, instead of relying on axioms of logic.
+//!
+//! 64bit CPUs are capable of checking logical proofs of 6 arguments (booleans)
+//! in O(1), because proofs can be interpreted as tautologies (true for all input)
+//! and `2^6 = 64`.
+//!
+//! This is done by replacing `bool` with `u64` and organizing inputs
+//! using bit patterns that simulate a truth table of 6 arguments.
+//!
+//! To extend to 10 arguments, `T` and `F` are used to alternate the 4 extra arguments.
 
 /// 0xaaaa_aaaa_aaaa_aaaa
 pub const P0: u64 = 0b10101010_10101010_10101010_10101010_10101010_10101010_10101010_10101010;
@@ -108,39 +167,93 @@ pub fn prove10<F: Fn(u64, u64, u64, u64, u64, u64, u64, u64, u64, u64) -> u64>(f
 /// In logical terminology this corresponds to a proposition.
 pub fn prop(a: bool) -> u64 {if a {T} else {F}}
 
+/// Ignores argument, always returning `false`.
 pub fn false_1(_: u64) -> u64 {0}
+/// If input is `true`, returns `false` and vice versa.
 pub fn not(a: u64) -> u64 {!a}
+/// Returns argument.
 pub fn id(a: u64) -> u64 {a}
+/// Ignores argument, always returning `true`.
 pub fn true_1(_: u64) -> u64 {T}
 
+/// Ignores both arguments, returning `false` for all inputs.
 pub fn false_2(_: u64, _: u64) -> u64 {0}
+/// Returns `true` if all arguments are `true`.
 pub fn and(a: u64, b: u64) -> u64 {a & b}
+/// Returns `true` if at least one argument is `true`.
 pub fn or(a: u64, b: u64) -> u64 {a | b}
+/// Returns `true` if only one argument is `true`.
 pub fn xor(a: u64, b: u64) -> u64 {a ^ b}
+/// Returns `true` if arguments are equal.
 pub fn eq(a: u64, b: u64) -> u64 {!(a ^ b)}
+/// First argument implies the second.
 pub fn imply(a: u64, b: u64) -> u64 {!a | b}
+/// Ignores both arguments, returning `true` for all inputs.
 pub fn true_2(_: u64, _: u64) -> u64 {T}
 
+/// Ignores all 3 arguments, returning `false` for all inputs.
 pub fn false_3(_: u64, _: u64, _: u64) -> u64 {0}
+/// Ignores all 4 arguments, returning `false` for all inputs.
 pub fn false_4(_: u64, _: u64, _: u64, _: u64) -> u64 {0}
+/// Ignores all 5 arguments, returning `false` for all inputs.
 pub fn false_5(_: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {0}
+/// Ignores all 6 arguments, returning `false` for all inputs.
 pub fn false_6(_: u64, _: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {0}
+/// Ignores all 7 arguments, returning `false` for all inputs.
+pub fn false_7(_: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {0}
+/// Ignores all 8 arguments, returning `false` for all inputs.
+pub fn false_8(_: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {0}
+/// Ignores all 9 arguments, returning `false` for all inputs.
+pub fn false_9(_: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {0}
+/// Ignores all 10 arguments, returning `false` for all inputs.
+pub fn false_10(
+    _: u64, _: u64, _: u64, _: u64, _: u64,
+    _: u64, _: u64, _: u64, _: u64, _: u64
+) -> u64 {0}
 
+/// Ignores all 3 arguments, returning `true` for all inputs.
+pub fn true_3(_: u64, _: u64, _: u64) -> u64 {T}
+/// Ignores all 4 arguments, returning `true` for all inputs.
+pub fn true_4(_: u64, _: u64, _: u64, _: u64) -> u64 {T}
+/// Ignores all 5 arguments, returning `true` for all inputs.
+pub fn true_5(_: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {T}
+/// Ignores all 6 arguments, returning `true` for all inputs.
+pub fn true_6(_: u64, _: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {T}
+/// Ignores all 7 arguments, returning `true` for all inputs.
+pub fn true_7(_: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {T}
+/// Ignores all 8 arguments, returning `true` for all inputs.
+pub fn true_8(_: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {T}
+/// Ignores all 9 arguments, returning `true` for all inputs.
+pub fn true_9(_: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64, _: u64) -> u64 {T}
+/// Ignores all 10 arguments, returning `true` for all inputs.
+pub fn true_10(
+    _: u64, _: u64, _: u64, _: u64, _: u64,
+    _: u64, _: u64, _: u64, _: u64, _: u64
+) -> u64 {T}
+
+/// And and relation of 3 argument.
 pub fn and3(a: u64, b: u64, c: u64) -> u64 {and(and(a, b), c)}
+/// An and relation of 4 arguments.
 pub fn and4(a: u64, b: u64, c: u64, d: u64) -> u64 {and(and(a, b), and(c, d))}
+/// An and relation of 5 arguments.
 pub fn and5(a: u64, b: u64, c: u64, d: u64, e: u64) -> u64 {and(and(a, b), and3(c, d, e))}
+/// An and relation of 6 arguments.
 pub fn and6(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64) -> u64 {
     and(and3(a, b, c), and3(d, e, f))
 }
+/// An and relation of 7 arguments.
 pub fn and7(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64) -> u64 {
     and(and4(a, b, c, d), and3(e, f, g))
 }
+/// An and relation of 8 arguments.
 pub fn and8(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64, h: u64) -> u64 {
     and(and4(a, b, c, d), and4(e, f, g, h))
 }
+/// An and relation of 9 arguments.
 pub fn and9(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64, h: u64, i: u64) -> u64 {
     and(and5(a, b, c, d, e), and4(f, g, h, i))
 }
+/// An and relation of 10 arguments.
 pub fn and10(
     a: u64, b: u64, c: u64, d: u64, e: u64,
     f: u64, g: u64, h: u64, i: u64, j: u64
@@ -148,19 +261,27 @@ pub fn and10(
     and(and5(a, b, c, d, e), and5(f, g, h, i, j))
 }
 
+/// An or relation of 3 arguments.
 pub fn or3(a: u64, b: u64, c: u64) -> u64 {or(or(a, b), c)}
+/// An or relation of 4 arguments.
 pub fn or4(a: u64, b: u64, c: u64, d: u64) -> u64 {or(or(a, b), or(c, d))}
+/// An or relation of 5 arguments.
 pub fn or5(a: u64, b: u64, c: u64, d: u64, e: u64) -> u64 {or(or3(a, b, c), or(d, e))}
+/// An or relation of 6 arguments.
 pub fn or6(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64) -> u64 {or(or3(a, b, c), or3(d, e, f))}
+/// An or relation of 7 arguments.
 pub fn or7(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64) -> u64 {
     or(or4(a, b, c, d), or3(e, f, g))
 }
+/// An or relation of 8 arguments.
 pub fn or8(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64, h: u64) -> u64 {
     or(or4(a, b, c, d), or4(e, f, g, h))
 }
+/// An or relation of 9 arguments.
 pub fn or9(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64, h: u64, i: u64) -> u64 {
     or(or5(a, b, c, d, e), or4(f, g, h, i))
 }
+/// An or relation of 10 arguments.
 pub fn or10(
     a: u64, b: u64, c: u64, d: u64, e: u64,
     f: u64, g: u64, h: u64, i: u64, j: u64
@@ -168,21 +289,29 @@ pub fn or10(
     or(or5(a, b, c, d, e), or5(f, g, h, i, j))
 }
 
+/// An xor relation of 3 arguments.
 pub fn xor3(a: u64, b: u64, c: u64) -> u64 {xor(xor(a, b), c)}
+/// An xor relation of 4 arguments.
 pub fn xor4(a: u64, b: u64, c: u64, d: u64) -> u64 {xor(xor(a, b), xor(c, d))}
+/// An xor relation of 5 arguments.
 pub fn xor5(a: u64, b: u64, c: u64, d: u64, e: u64) -> u64 {xor(xor3(a, b, c), xor(d, e))}
+/// An xor relation of 6 arguments.
 pub fn xor6(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64) -> u64 {
     xor(xor3(a, b, c), xor3(d, e, f))
 }
+/// An xor relation of 7 arguments.
 pub fn xor7(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64) -> u64 {
     xor(xor4(a, b, c, d), xor3(e, f, g))
 }
+/// An xor relation of 8 arguments.
 pub fn xor8(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64, h: u64) -> u64 {
     xor(xor4(a, b, c, d), xor4(e, f, g, h))
 }
+/// An xor relation of 9 arguments.
 pub fn xor9(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64, h: u64, i: u64) -> u64 {
     xor(xor5(a, b, c, d, e), xor4(f, g, h, i))
 }
+/// An xor relation of 10 arguments.
 pub fn xor10(
     a: u64, b: u64, c: u64, d: u64, e: u64,
     f: u64, g: u64, h: u64, i: u64, j: u64
@@ -190,26 +319,34 @@ pub fn xor10(
     xor(xor5(a, b, c, d, e), xor5(f, g, h, i, j))
 }
 
+/// An imply chain of 3 arguments.
 pub fn imply3(a: u64, b: u64, c: u64) -> u64 {and(imply(a, b), imply(b, c))}
+/// An imply chain of 4 arguments.
 pub fn imply4(a: u64, b: u64, c: u64, d: u64) -> u64 {
     and3(imply(a, b), imply(b, c), imply(c, d))
 }
+/// An imply chain of 5 arguments.
 pub fn imply5(a: u64, b: u64, c: u64, d: u64, e: u64) -> u64 {
     and4(imply(a, b), imply(b, c), imply(c, d), imply(d, e))
 }
+/// An imply chain of 6 arguments.
 pub fn imply6(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64) -> u64 {
     and5(imply(a, b), imply(b, c), imply(c, d), imply(d, e), imply(e, f))
 }
+/// An imply chain of 7 arguments.
 pub fn imply7(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64) -> u64 {
     and6(imply(a, b), imply(b, c), imply(c, d), imply(d, e), imply(e, f), imply(f, g))
 }
+/// An imply chain of 8 arguments.
 pub fn imply8(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64, h: u64) -> u64 {
     and7(imply(a, b), imply(b, c), imply(c, d), imply(d, e), imply(e, f), imply(f, g), imply(g, h))
 }
+/// An imply chain of 9 arguments.
 pub fn imply9(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64, g: u64, h: u64, i: u64) -> u64 {
     and8(imply(a, b), imply(b, c), imply(c, d), imply(d, e),
          imply(e, f), imply(f, g), imply(g, h), imply(h, i))
 }
+/// An imply chain of 10 arguments.
 pub fn imply10(
     a: u64, b: u64, c: u64, d: u64, e: u64,
     f: u64, g: u64, h: u64, i: u64, j: u64,
@@ -218,11 +355,16 @@ pub fn imply10(
          imply(e, f), imply(f, g), imply(g, h), imply(h, i), imply(i, j))
 }
 
+/// A boolean function of one argument.
 pub type Pred1 = fn(u64) -> u64;
+/// A boolean function (transformed) of two arguments.
 pub type Pred2 = fn(u64, u64) -> u64;
 
+/// Implemented by types to use with `all` and `any`.
 pub trait Enumerable: Sized {
+    /// Returns start value.
     fn start() -> Self;
+    /// Increases value.
     fn inc(&self) -> Option<Self>;
 }
 
