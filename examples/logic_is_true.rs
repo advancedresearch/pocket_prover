7199a378bcfaf8b185a81081af57bc479c4d76d7
Sven Nilsen <bvssvni@gmail.com>
2017-12-11 08:00:12 +0100

    Added example showing that if logic is false, then logic is true

---
+++ examples/logic_is_true.rs
@@ -0,0 +1,41 @@
+extern crate pocket_prover;
+
+use pocket_prover::*;
+
+fn main() {
+    println!("If logic is false, then logic is true:");
+    println!("(T -> F) -> (T -> T)");
+    println!("Result: {}", prove1(|_| {
+        imply(imply(T, F), imply(T, T))
+    }));
+    println!("");
+    println!("However, the opposite is not true:");
+    println!("(T -> T) -> (T -> F)");
+    println!("Result: {}", prove1(|_| {
+        imply(imply(T, T), imply(T, F))
+    }));
+    println!("");
+    println!("Can prove that if logic is true, then is not true that logic is false:");
+    println!("(T -> T) -> not(T -> F)");
+    println!("Result: {}", prove1(|_| {
+        imply(imply(T, T), not(imply(T, F)))
+    }));
+    println!("");
+    println!("Combining the above shows to show that logic must be true:");
+    println!("( (T -> F) -> (T -> T) ) & ( (T -> T) -> not(T -> F) )");
+    println!("------------------------------------------------------");
+    println!("T -> T");
+    println!("Result: {}", prove1(|_| {
+        imply(
+            and(
+                imply(imply(T, F), imply(T, T)),
+                imply(imply(T, T), not(imply(T, F)))
+            ),
+            imply(T, T)
+        )
+    }));
+    println!("");
+    println!("Actually, we are only asserting that true implies true.");
+    println!("Still, the same thing happens for all proofs");
+    println!("It just becomes clearer when the proofs depends on no variables.");
+}
