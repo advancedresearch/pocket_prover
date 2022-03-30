4c129cd6f40147bff7bec7be5112f5afcc5a5595
Sven Nilsen <bvssvni@gmail.com>
2022-03-30 14:16:31 +0200

    Added "examples/quality_creation_theorem"

---
+++ examples/quality_creation_theorem.rs
@@ -0,0 +1,41 @@
+/*
+
+This example shows that the Creation Theorem is not a tautology
+in the model of path semantical quality that resembles quantum logic.
+
+This means that this model of quality gives rise to a new logic:
+
+   Path Semantical Quantum Propositional Logic (PSQ)
+
+*/
+
+use pocket_prover::*;
+
+fn main() {
+    println!("Creation Theorem: {}", measure(10, || prove!(&mut |a, b, a2, b2| {
+        creation(a, b, a2, b2)
+    })));
+
+    println!("");
+    for i in 1..10 {
+        for _ in 0..50 {
+            if measure(i, || prove!(&mut |a, b, c, d| creation(a, b, c, d))) {
+                print!("1");
+            } else {
+                print!("0");
+            }
+        }
+        println!("");
+    }
+}
+
+pub fn creation(a: u64, b: u64, a2: u64, b2: u64) -> u64 {
+    imply(
+        and!(
+            ps_core(a, b, a2, b2),
+            not(a),
+            imply(b, b2),
+        ),
+        imply(a2, b2)
+    )
+}
